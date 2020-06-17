c2ad36823de309665e4727569e2240f9dd55b5c0
Maxime Mérian <maxime@merian.me>
2020-06-17 08:58:10 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+BUILD_OPTIONS=''
+TAG_VERSION='no'
+PUSH_TAG='no'
+IMAGE_NAME=mmerian/thruk
+
+while getopts "ntp" option; do
+    case $option in
+        n)
+            BUILD_OPTIONS='--no-cache'
+        ;;
+        t)
+            TAG_VERSION='yes'
+        ;;
+        p)
+            PUSH_TAG='yes'
+        ;;
+    esac
+done
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+docker build $DIR -t ${IMAGE_NAME}:latest $BUILD_OPTIONS
+
+if [ "$TAG_VERSION" == "yes" ]; then
+    # Get version number, in order to create tag
+    IMAGE_VER=`docker run ${IMAGE_NAME}:latest apt-cache madison thruk|cut -f 2 -d '|'|tr -d [:space:]`
+    echo "Tagging version $IMAGE_VER"
+    docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:$IMAGE_VER
+    if [ "$PUSH_TAG" == "yes" ]; then
+        echo "Pushing tag latest"
+        docker push "${IMAGE_NAME}:latest"
+        echo "Pushing tag $IMAGE_VER"
+        docker push "${IMAGE_NAME}:$IMAGE_VER"
+    fi
+fi
